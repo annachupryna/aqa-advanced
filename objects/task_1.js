bf10e2153d237e2e2e1f76559029d0c9c0a8ac0c
Anna Chupryna <anna.chupryna@cashberry.com.ua>
2026-02-01 08:46:11 +0100

    homework 9

---
+++ objects/task_1.js
@@ -0,0 +1,22 @@
+`
+Завдання 1
+Створіть порожній об'єкт з ім'ям student.
+Додайте до об'єкта student наступні властивості:
+firstName: рядок, представляє ім'я студента.
+lastName: рядок, представляє прізвище студента.
+age: число, представляє вік студента.
+courses: масив, представляє список курсів, які він вивчає (наприклад, ["Математика", "Історія", "Програмування"]).
+Виведіть об'єкт student в консоль.
+`
+
+const student = {};
+student.firstName = 'John';
+student.lastName = 'Smith';
+student.age = 22;
+student.courses = ['Math', 'Biology'];
+
+console.log(student);
+
+for (let key in student) {
+    console.log(key + ': ' + student[key]);
+}
---
+++ objects/task_5.js
@@ -0,0 +1,21 @@
+`
+Завдання 5
+Створіть масив об'єктів users де обєкти мають довільні властивості (наприклад, name, email, age, тощо).
+Використовуючи цикл for...of, переберіть всі елементи масиву та виведіть їхні значення в консоль.
+Зробіть деструктуризацію в циклі
+`
+
+
+const users = [
+    { name: 'John', age: 22 },
+    { name: 'Alice', age: 19 },
+    { name: 'Andrew', age: 20 }
+]
+
+for (const user of users) {
+    console.log(`${user.name} is ${user.age} years old`);
+}
+
+for (const { name, age } of users) {
+    console.log(`${name} is ${age} years old`);
+}
